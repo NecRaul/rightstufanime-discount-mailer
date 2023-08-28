33f7155a2d526ba5a0d36d17cab131d500912a0c
NecRaul <necraul2001@gmail.com>
2023-08-28 04:04:51 +0400

    Variables

---
+++ variables.py
@@ -0,0 +1,24 @@
+# mail port
+port = 465  # you may have to change this if you're using starttls
+
+# gmail smtp
+smtp_server = "smtp.gmail.com"
+
+# account you'll be sending mail with
+sender_email = ""
+
+# app password
+# you have to have 2FA activated in order to create an app password
+password = ""
+
+# account you'll be receiving mail with a.k.a. the important one
+receiver_email = ""
+
+# email subject
+email_subject = "Discounted Items"
+
+# base price to compare items to
+base_price = 11.19
+
+# public wishlist
+website = ""
